b13b0d95b27b3b8774c5c74ec1754df2fb653145
EMMANUEL BUGINGO <emmanuelbugingo@stu.xmu.edu.cn>
2025-01-27 13:25:12 +0200

    Added new file

---
+++ dom.js
@@ -0,0 +1,3 @@
+var paragraphs = document.getElementsByTagName("p");
+    alert("Content in the second paragraph is " + paragraphs[1].innerHTML);
+    document.getElementById("second").innerHTML = "The orginal message is changed.";
